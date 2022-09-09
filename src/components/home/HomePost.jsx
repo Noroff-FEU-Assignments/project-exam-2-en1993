84e21bea0e1d669d2c55424a58a1424840826a74
en1993 <74547465+EN1993@users.noreply.github.com>
2022-09-09 19:49:56 +0200

    add

--- src/components/home/HomePost.jsx
+++ src/components/home/HomePost.jsx
@@ -45,20 +45,57 @@ export default function HomePost() {
   // for (let i = id  posts.id = id.0 && id.8 && id.9; i++) {
   //   if(i === id){ break; }
   // }
+
+  const slidesContainer = document.getElementById("slide-Container");
+  const imageSlide = document.querySelector(".image-slide");
+  const prevBtn = document.getElementById("slide-prev");
+  const nextBtn = document.getElementById("slide-next");
+
+  function galleryShowing(galleryShow) {
+    var shows = document.querySelectorAll(
+      `#${galleryShow.id} [role="list"] .show`
+    ); // Get an array of slides
+
+    var index = 0,
+      time = 6000;
+    shows[index].classList.add("activate");
+
+    setInterval(() => {
+      shows[index].classList.remove("activate");
+      index++;
+
+      if (index === shows.length) index = 0;
+      shows[index].classList.add("activate");
+    }, time);
+  }
+  // console.log({ galleryShows });
+  var galleryShows = document.querySelectorAll('[data-element="galleryShow"]');
+  galleryShows.forEach(galleryShowing);
+
   return (
     <>
-      {posts.map(function (post) {
-        return (
-          <div key={post.id}>
-            <Heading content={post.title.rendered} size="3"/>
-            
-            <div className="slidePic">
-              {" "}
-              <img src={post.source_url} alt={post.title.rendered} />
+      {posts
+        .filter(function (post) {
+          return post.id === 629 || post.id === 621 || post.id === 620;
+        })
+        .map(function (post) {
+          return (
+            <div key={post.id}>
+              <div id="showGallery" data-element="galleryShow">
+                <div role="list">
+                  <div className="show">
+                    <Heading content={post.title.rendered} size="3" />
+                    <img
+                      src={post.source_url}
+                      alt={post.title.rendered}
+                      className="slideImage"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </>
   );
 }
--- src/components/home/Index.jsx
+++ src/components/home/Index.jsx
@@ -2,12 +2,17 @@ import Footer from "../layout/footer/Footer";
 import Heading from "../layout/Heading";
 import HomePost from "./HomePost";
 
+
 const HomePage = () => {
   return (
     <>
       <main>
         <Heading content=" This is Home Page" />
-        <HomePost />
+        <section className="welcome-section">
+          <div className="slider-wrapper">
+            <HomePost />
+          </div>
+        </section>
       </main>
       <Footer />
     </>
