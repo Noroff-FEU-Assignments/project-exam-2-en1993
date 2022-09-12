0776e30c73c95aee88201b7a549e073997161ff1
en1993 <74547465+EN1993@users.noreply.github.com>
2022-09-12 15:27:19 +0200

    add

---
+++ src/components/home/Features.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { SECONDARY_API } from "../../constants/secondaryApi";
+import Heading from "../layout/Heading";
+import styles from "../home/Index.module.css";
+
+export default function FeaturedPost() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const response = await fetch(SECONDARY_API);
+
+        if (response.ok) {
+          const json = await response.json();
+          console.log("HOMEPOST:", json);
+          setPosts(json);
+        } else {
+          setError("An error occured");
+        }
+      } catch (error) {
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>An error occured : {error} </div>;
+  }
+
+  return (
+    <>
+      {posts
+        .filter(function (post) {
+          return post.id === 625 || post.id === 658 || post.id === 660;
+        })
+        .map(function (post) {
+          return (
+            <div key={post.id}>
+              <div className={styles.featuredContent}>
+                <img
+                  src={post.source_url}
+                  alt={post.title.rendered}
+                  className={styles.featuredPic}
+                />
+                <Heading content={post.title.rendered} size="3" />
+              </div>
+            </div>
+          );
+        })}
+    </>
+  );
+}
--- src/components/home/Index.jsx
+++ src/components/home/Index.jsx
@@ -2,6 +2,10 @@ import Footer from "../layout/footer/Footer";
 import Heading from "../layout/Heading";
 import HomePost from "./HomePost";
 import styles from "../home/Index.module.css";
+import { RiInformationFill } from "react-icons/ri";
+import { FaPhoneAlt } from "react-icons/fa";
+import { RiRestaurantFill } from "react-icons/ri";
+import FeaturedPost from "./Features";
 
 const HomePage = () => {
   return (
@@ -14,10 +18,35 @@ const HomePage = () => {
           </div>
         </section>
         <section>
+          <Heading content="Services" size="2" />
           <div className={styles.servicesContainer}>
-            <div className={styles.servicesContent}></div>
-            <div className={styles.servicesContent}></div>
-            <div className={styles.servicesContent}></div>
+            <div className={styles.servicesContent}>
+              <RiInformationFill className={styles.servicesIcons}/>
+              <Heading content="Information" size="3" />
+              <p>
+                Ask us what kind of offer we have. Whether it is activity or if
+                there is something needed
+              </p>
+            </div>
+            <div className={styles.servicesContent}>
+              <FaPhoneAlt className={styles.servicesIcons} />
+              <Heading content="Phone" size="3" />
+              <p>Call us for room services or if you need anything.</p>
+            </div>
+            <div className={styles.servicesContent}>
+              <RiRestaurantFill className={styles.servicesIcons} />
+              <Heading content="Restaurant" size="3" />
+              <p>
+                Lots to choose from in the breakfast meal and much more on offer
+                in our restaurant.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Heading content="Features"size="2"/>
+          <div className={styles.featuredContainer}>
+            <FeaturedPost/>
           </div>
         </section>
       </main>
